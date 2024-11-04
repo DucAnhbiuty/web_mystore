aebd53ce56ac1ce21dbb40e4427dd2b983b3d7b6
Anh-Minh21 <anhminhttz@gmail.com>
2024-11-05 00:44:28 +0700

    5:26:30

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -1,23 +1,33 @@
-import { getCollections } from '@/lib/action'
-import { Collection } from 'mongoose'
+import { getCollections } from '@/lib/actions'
+import Link from 'next/link'
 import Image from 'next/image'
-//import React from 'react'
-
 
 const Collections = async () => {
-  const Collections = await getCollections()
-  return (
-    <div >
-      <p className='text-heading1-bold'>Collections</p> 
-      <div>
-        {Collections.map((Collection: CollectionType) => (
-          <link href={'/Collection${collection._id}'}key={Collection._id}>
-            <Image src={Collection.image} alt={Collection.title} width={350} height={200} className='rounded-lg cursor-pointer'></Image>
-          </link>
-        ))}
-      </div>
-    </div>
-  )
-}
+    const collections = await getCollections()
+
+    return (
+        <div className='flex flex-col items-center gap-10 py-8 px-5'>
+            <p className='text-heading1-bold'>Collections</p>
+            {!collections || collections.length === 0 ? (
+                <p className="text-body-bold">No collections found</p>
+            ) : (
+                <div className="flex flex-wrap items-center justify-center gap-8">
+                    {collections.map((collection: CollectionType) => (
+                        <Link href={'/collections/${collection._id}'} key={collection._id}>
+                            <Image
+                                key={collection._id}
+                                src={collection.image}
+                                alt={collection.title}
+                                width={350}
+                                height={200}
+                                className="rounded-lg cursor-pointer"
+                            />
+                        </Link>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default Collections
