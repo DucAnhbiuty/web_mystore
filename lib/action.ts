a7f8a642d964befbcf4c0a91990c1ff1969a663e
Duc Anh <DucAnhbiuty>
2024-11-13 11:38:31 +0700

    update

--- lib/action.ts
+++ lib/action.ts
@@ -1,10 +1,21 @@
 export const getCollections = async () => {
-  const collections = await fetch('${process.env.NEXT_PUBLIC_API_URL}/collections')
-  return await collections.json()
-}
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/collections`);
+  
+  if (!response.ok) {
+    console.error("Failed to fetch collections:", response.status, response.statusText);
+    return null; // Hoặc xử lý lỗi tùy ý
+  }
+
+  try {
+    return await response.json();
+  } catch (error) {
+    console.error("Response is not valid JSON:", error);
+    return null;
+  }
+}; //Check truy cập api
 
 export const getProducts= async () => {
-  const products = await fetch('${process.env.NEXT_PUBLIC_API_URL}/products')
+  const products = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`)
   return await products.json()
 }
 
